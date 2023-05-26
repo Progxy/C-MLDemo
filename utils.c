c61a6f757a09f85c005d873bfadc70d75e9fefce
TheProgxy <theprogxy@gmail.com>
2023-05-26 23:08:16 +0200

    Revert "final fix"
    This reverts commit 1dcabaebb09565da4909af0fcfa00a47daf8d89b.

--- utils.c
+++ utils.c
@@ -16,15 +16,3 @@ float rand_float() {
 float sigmoidf(float x) {
     return 1.f / (1.f + expf(-x));
 }
-
-float absf(float x) {
-    return x >= 0 ? x : -x;
-}
-
-float reluf(float x) {
-    return (x + absf(x)) / 2;
-}
-
-float preluf(float x, float alpha) {
-    return x >= 0 ? x : (alpha * x);
-}
